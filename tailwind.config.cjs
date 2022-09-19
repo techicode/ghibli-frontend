be7993b734ebf72f7d58d33deab3ada38e847215
techicode <techicode@outlook.com>
2022-09-19 00:23:58 -0300

    talwind initial configuration

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      eggsheld: '#F4FFD7',
+      'very-light-pink': '#FFEBE0',
+      ice: '#DBF7E4',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
