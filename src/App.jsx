90e3da66e1a2f6ffe73be778be40fc29003dbe53
techicode <techicode@outlook.com>
2022-09-20 14:12:57 -0300

    added axios and fetch to /movie end point

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import MovieRow from "./components/MovieRow";
+import axios from 'axios';
+
+import { useEffect, useState } from 'react';
+
+import MovieRow from './components/MovieRow';
+
+const fetchMovies = async (setMovies) => {
+  const headersList = {
+    Accept: '*/*',
+  };
+
+  const reqOptions = {
+    url: 'http://localhost:3000/movies/',
+    method: 'GET',
+    headers: headersList,
+  };
+
+  const response = await axios.request(reqOptions);
+  console.log(response.data);
+  setMovies(response.data);
+};
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    if (movies.length <= 0) {
+      fetchMovies(setMovies);
+    }
+  });
+
   return (
     <div className="App">
       <div className="left-side flex min-h-screen ">
         <div className="bg-eggsheld flex-none w-1/3 caveat text-7xl max-w-xl text-center ">
-          <div class="flex-col flex place-content-around h-full">
+          <div className="flex-col flex place-content-around h-full">
             <div className="mb-10">Logo</div>
             <div className="h-1/5 underline decoration-wavy decoration-violet-200 decoration-4 underline-offset-8 text-violet-400">
               Movies
@@ -16,29 +44,26 @@ function App() {
           </div>
         </div>
         <div className="bg-ice flex-1 flex-col flex  justify-around rancho">
-          
-         <MovieRow />
-         <MovieRow />
-         <MovieRow />
-         <MovieRow />
-
+          {movies.map((movie) => (
+            <MovieRow key={movie.id} movie={movie} />
+          ))}
           <div className="h-min">
-            <ol class="flex justify-center text-xs font-medium gap-1">
+            <ol className="flex justify-center text-xs font-medium gap-1">
               <li>
                 <a
                   href="/?page=1"
-                  class="inline-flex items-center justify-center w-8 h-8 border border-gray-100 rounded"
+                  className="inline-flex items-center justify-center w-8 h-8 border border-gray-100 rounded"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="w-3 h-3"
+                    className="w-3 h-3"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     />
                   </svg>
                 </a>
@@ -47,20 +72,20 @@ function App() {
               <li>
                 <a
                   href="/?page=1"
-                  class="block w-8 h-8 text-center border border-gray-100 rounded leading-8"
+                  className="block w-8 h-8 text-center border border-gray-100 rounded leading-8"
                 >
                   1
                 </a>
               </li>
 
-              <li class="block w-8 h-8 text-center text-white bg-blue-600 border-blue-600 rounded leading-8">
+              <li className="block w-8 h-8 text-center text-white bg-blue-600 border-blue-600 rounded leading-8">
                 2
               </li>
 
               <li>
                 <a
                   href="/?page=3"
-                  class="block w-8 h-8 text-center border border-gray-100 rounded leading-8"
+                  className="block w-8 h-8 text-center border border-gray-100 rounded leading-8"
                 >
                   3
                 </a>
@@ -69,7 +94,7 @@ function App() {
               <li>
                 <a
                   href="/?page=4"
-                  class="block w-8 h-8 text-center border border-gray-100 rounded leading-8"
+                  className="block w-8 h-8 text-center border border-gray-100 rounded leading-8"
                 >
                   4
                 </a>
@@ -78,18 +103,18 @@ function App() {
               <li>
                 <a
                   href="/?page=3"
-                  class="inline-flex items-center justify-center w-8 h-8 border border-gray-100 rounded"
+                  className="inline-flex items-center justify-center w-8 h-8 border border-gray-100 rounded"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="w-3 h-3"
+                    className="w-3 h-3"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     />
                   </svg>
                 </a>
--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,67 +1,50 @@
-const MovieRow = () => {
-
+const MovieRow = ({ movie }) => {
   return (
     <div className="rounded-lg  h-full grid grid-cols-5 my-5 ">
-    <img
-      src="https://www.ghibli.jp/images/kazetachinu.jpg"
-      alt=""
-      className="object-scale-down h-40 w-96"
-    />
-    <div class="sm:col-span-2">
-      <ul class="flex gap-1">
-        <li class="inline-block px-3 py-1 text-xs font-semibold text-white bg-blue-600 rounded-full">
-          Notice
-        </li>
+      <img
+        src={movie.cover_img}
+        alt={`${movie.title} cover picture`}
+        className="object-scale-down h-40 w-96"
+      />
+      <div className="sm:col-span-2">
+        <h1 className=" text-2xl text-gray-800">{movie.title}</h1>
+        <p className="text-gray-700">{movie.summary}</p>
+      </div>
+      <div className="flex-none col-span-2">
+        <strong className="inline-flex items-center gap-2 px-5 py-2 bg-very-light-pink rounded-full">
+          <img
+            alt="Man"
+            src={movie.director.picture}
+            className="object-cover w-12 h-12 -ml-2 rounded-full"
+          />
+          <ul className="flex-col gap-1">
+            <li className="inline-block px-3 py-1 text-xs font-semibold text-gray-500 bg-eggsheld rounded-full">
+              Director
+            </li>
+            <li className="text-xl px-3 py-1 font-medium text-gray-900">
+              {movie.director.name}
+            </li>
+          </ul>
+        </strong>
+        <strong className="inline-flex items-center gap-2 px-5 py-2 bg-eggsheld rounded-full">
+          <img
+            alt="Man"
+            src={movie.producer.picture}
+            className="object-cover w-12 h-12 -ml-2 rounded-full"
+          />
 
-        <li class="inline-block px-3 py-1 text-xs font-semibold text-white bg-blue-600 rounded-full">
-          Information
-        </li>
-      </ul>
-
-      <h1 className=" text-2xl text-gray-800">The Wind Rises</h1>
-
-      <p className="text-gray-700">
-        A lifelong love of flight inspires Japanese aviation engineer
-        Jiro Horikoshi (Hideaki Anno), whose storied career includes the
-        creation of the A6M World War II fighter plane.
-      </p>
+          <ul className="flex-col gap-1">
+            <li className="inline-block px-3 py-1 text-xs font-semibold text-gray-500 bg-very-light-pink rounded-full">
+              Producer
+            </li>
+            <li className="text-xl px-3 py-1 font-medium text-gray-900">
+              {movie.producer.name}
+            </li>
+          </ul>
+        </strong>
+      </div>
     </div>
-    <div className="flex-none col-span-2">
-      <strong class="inline-flex items-center gap-2 px-5 py-2 bg-very-light-pink rounded-full">
-        <img
-          alt="Man"
-          src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-          class="object-cover w-12 h-12 -ml-2 rounded-full"
-        />
-        <ul class="flex-col gap-1">
-          <li class="inline-block px-3 py-1 text-xs font-semibold text-gray-500 bg-eggsheld rounded-full">
-            Director
-          </li>
-          <li class="text-xl px-3 py-1 font-medium text-gray-900">
-            Eric Frusciante
-          </li>
-        </ul>
-      </strong>
-      <strong class="inline-flex items-center gap-2 px-5 py-2 bg-eggsheld rounded-full">
-        <img
-          alt="Man"
-          src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-          class="object-cover w-12 h-12 -ml-2 rounded-full"
-        />
-
-        <ul class="flex-col gap-1">
-          <li class="inline-block px-3 py-1 text-xs font-semibold text-gray-500 bg-very-light-pink rounded-full">
-            Producer
-          </li>
-          <li class="text-xl px-3 py-1 font-medium text-gray-900">
-            Eric Frusciante
-          </li>
-        </ul>
-      </strong>
-    </div>
-  </div>
-  
-  )
-}
+  );
+};
 
 export default MovieRow;
